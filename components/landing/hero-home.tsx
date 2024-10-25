f8c4165fd4b444f5de71e7783c7528b9499657ca
heshay <m.venkat@outlook.com>
2024-10-25 13:47:40 +0100

    Update hero-home.tsx

--- components/landing/hero-home.tsx
+++ components/landing/hero-home.tsx
@@ -44,39 +44,19 @@ const HeroHome = () => {
 
           {/* Section header */}
           <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-6xl md:text-7xl font-extrabold leading-tight mb-4" data-aos="zoom-y-out">Immersive <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-900 to-purple-400">leadership development</span> personalised for your team</h1>
+            <h1 className="text-6xl md:text-7xl font-extrabold leading-tight mb-4" data-aos="zoom-y-out">Retain talent with <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-900 to-purple-400">internal mobility</span> coaching</h1>
             <div className="max-w-3xl mx-auto">
               <p className="text-xl md:text-2xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">
                 Help your people exceed their potential, so they can unlock yours
               </p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center mb-4" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
-                  <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://exec.coach">Try now</a>
+                  <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://exec.coach">Free Trial</a>
                 </div>
                 <div>
-                  <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="https://exec.coach">Let's chat</a>
+                  <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="https://exec.coach">Let's Chat</a>
                 </div>
               </div>
-            </div>
-          </div>
-
-          {/* Hero image */}
-          <div>
-            <div className="relative flex justify-center mb-8 max-w-3xl mx-auto" data-aos="zoom-y-out" data-aos-delay="450">
-              <div className="flex flex-col justify-center w-full shadow-md border rounded">
-                {iframeHidden ? (
-                  <img className="w-full mx-auto" src="assets/demo.png" alt="Hero" />
-                ) : (
-                  <iframe className="w-full h-[500px]" src="https://exec.coach"></iframe>
-                )}
-              </div>
-              <button className={`absolute top-full flex items-center transform -translate-y-1/2 bg-white rounded-full font-medium group p-4 shadow-lg ${iframeHidden ? 'block' : 'hidden'}`} onClick={(e) => { e.preventDefault(); e.stopPropagation(); setIframeHidden(!iframeHidden); }} aria-controls="modal">
-                <svg className="w-6 h-6 fill-current text-gray-400 group-hover:text-blue-600 shrink-0" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm0 2C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12z" />
-                  <path d="M10 17l6-5-6-5z" />
-                </svg>
-                <span className="ml-3">Click to try yourself</span>
-              </button>
             </div>
           </div>
         
