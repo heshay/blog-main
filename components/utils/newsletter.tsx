f24c03f2382a9081319ac6ae3f3be5f3d8d9c91c
heshay <m.venkat@outlook.com>
2024-10-25 20:03:24 +0100

    Update newsletter.tsx

--- components/utils/newsletter.tsx
+++ components/utils/newsletter.tsx
@@ -1,6 +1,6 @@
 
 export async function subscribeCustomerIo(email: String) {
-  const resp = await fetch("https://track.customer.io/api/v1/forms/newsletter_form/submit", {
+  const resp = await fetch("https://hooks.airtable.com/workflows/v1/genericWebhook/appm0Sh6EQZ4NZXjL/wflEVQkL7spSPXecQ/wtr2HqH39AP7HX1Fd", {
     method: "POST",
     headers: {
       "Authorization": `Basic ${process.env.NEXT_PUBLIC_CUSTOMER_IO_FORM_AUTH}`,
